ace9faa0991f03107eed9801676c15977138af4a
Soobin Yang <yasoo8449@gmail.com>
2024-06-28 08:34:07 -0700

    remove redundant prompts

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ promptsDict = {
     "extract" : "\nExtract only the article from the above text.",
     "isValidArticle" : "\nIs above text valid article?\nIf so reply True. If not reply False.",
     "isRelatedToJapan" : "\nIs the above text related to Japan from Japan's view?\nIf so reply True. If not reply False.",
-    "rewrite" : "\nRewrite the above text with Japan as topic.",
-    "summarize" : "\nSummarize the above text in 70 words.",
     "translate" : "\n日本語に翻訳してください。",
     "finalizeContent" : "\nーツイート文章に直して\nー日本を中心にして\nー絵文字は使わないで\nー80文字ぐらいにして",
     "finalizeTitle" : "\n文章のタイトルを10文字ぐらいで書いて",
-    "finalizeHeader" : "\5文字以内の状態を一つの単語で書いて",
+    "finalizeHeader" : "\n5文字以内の状態を一つの単語で書いて",
     "isValidArticleJapanese" : "\n上の文章は記事として適切ですか?もしそうならTrue、そうでないならFalseを返してください。",
     "extractJapanese" : "\n上の文章から記事を抜き出して",
     "sadTweet" : "\n上の文章について日本への批判を100文字ぐらいで男性口調で書いて。",
@@ -29,9 +27,9 @@ articles = [{
 today_utc = datetime.datetime.now(datetime.timezone.utc).date()
 
 def create_neutral_tweet(article):
-    article["final_content"] = runModel("pro", article["translated_summarized_content"] + promptsDict["finalizeContent"])
-    article["final_title"] = runModel("pro", article["translated_summarized_content"] + promptsDict["finalizeTitle"])
-    article["final_header"] = runModel("flash", article["translated_summarized_content"] + promptsDict["finalizeHeader"])
+    article["final_content"] = runModel("pro", article["translated_content"] + promptsDict["finalizeContent"])
+    article["final_title"] = runModel("pro", article["final_content"] + promptsDict["finalizeTitle"])
+    article["final_header"] = runModel("flash", article["translated_content"] + promptsDict["finalizeHeader"])
     article["final_header"] = article["final_header"].replace("\n", "")
     article["final_header"] = article["final_header"].replace(" ", "")
     article["final_header"] = '【' + article["final_header"] + '】'
@@ -53,9 +51,7 @@ def neutral_main():
             if "True" not in runModel("flash", article["content"] + promptsDict["isValidArticle"]):
                 continue
 
-            article["rewritten_text"] = runModel("flash", article["title"] + article["content"] + promptsDict["rewrite"])
-            article["summarized_text"] = runModel("flash", article["rewritten_text"] + promptsDict["summarize"])
-            article["translated_summarized_content"] = runModel("flash", article["summarized_text"] + promptsDict["translate"])
+            article["translated_content"] = runModel("flash", article["content"] + promptsDict["translate"])
 
             create_neutral_tweet(article)
         except Exception as e:
